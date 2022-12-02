377d5839ad205bd10e28755153eb2f95cef676ac
User <dg21stew@gmail.com>
2022-12-02 00:01:40 -0800

    added getNatoAlpha method

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
             System.out.println("value is 1 \n");
         } else if (value == 2) {
             System.out.println("value is 2 \n");
-        }else {
+        } else {
             System.out.println("was not 1 or 2 \n");
         }
 
@@ -18,17 +18,48 @@ public class Main {
         System.out.println("Enter a letter: ");
         String action = scanner.nextLine();
         switch (action) {
-            case "az" -> System.out.println("You chose az");
-            case "b" -> System.out.println("You chose b");
-            case "c" -> System.out.println("You chose c");
-            case "d" -> System.out.println("You chose d");
-            case "e" -> System.out.println("You chose e");
-            case "f" -> System.out.println("You chose f");
+            case "az" -> System.out.println("You chose az \n");
+            case "b" -> System.out.println("You chose b \n");
+            case "c" -> System.out.println("You chose c \n");
+            case "d" -> System.out.println("You chose d \n");
+            case "e" -> System.out.println("You chose e \n");
+            case "f" -> System.out.println("You chose f \n");
             case "g", "h", "i", "j" -> {
-                System.out.println("You chose g, h, i, or j");
-                System.out.println("The letter is: " + action);
+                System.out.println("You chose g, h, i, or j \n");
+                System.out.println("The letter is: " + action +"\n");
             }
-            default -> System.out.println("Chosen character not listed");
+            default -> System.out.println("Chosen character not listed\n");
         }
+
+
+        String month  = "OCTOBER";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarter\n");
+
+        char alpha  = 'e';
+        System.out.println(alpha + " in NATO code is " + getNatoAlpha(alpha));
+    }
+
+
+
+    public static String getQuarter(String month) {
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> "bad";
+        };
+    }
+
+
+    public static String getNatoAlpha(char character) {
+        return switch (character) {
+            case 'A' -> "Alpha";
+            case 'B' -> "Bravo";
+            case 'C' -> "Charlie";
+            case 'D' -> "Delta";
+            case 'E' -> "Echo";
+            default -> "bad";
+        };
     }
 }
